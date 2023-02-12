fb4c30efec8313b5cae92515a8b2e63a426e2a36
Garhoogin <declan.c.moore@gmail.com>
2023-02-12 00:19:57 -0600

    Add support for using official headers

--- test/source/vblankhook.c
+++ test/source/vblankhook.c
@@ -1,13 +1,11 @@
-#include <nds.h>
-
-#define reg_KEYINPUT (*(vu16 *)0x04000130)
+#include <nitro.h>
 
 extern void driver_2068A64(void *driver, int a2, int a3);
 
 void RaceVBlankIntrHook(void) {
 	void *driver = *(void **) 0x0217D028;
 	
-	if ((~reg_KEYINPUT) & KEY_SELECT) {
+	if ((~reg_PAD_KEYINPUT) & REG_PAD_KEYINPUT_SEL_MASK) {
 		driver_2068A64(driver, 2, 1);
 	} else {
 		driver_2068A64(driver, 0, 1);
